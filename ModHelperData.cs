7952d38b066b8c076e0f60551b0038bfefa8044f
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2022-11-07 21:14:38 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -5,7 +5,7 @@
         public const string Version = "1.0.0";
         public const string Name = "Everything Is Free";
         public const string Description =
-            "Makes all upgrades and towers free!";
+            "This mod makes all upgrades and towers free!";
 
         public const string RepoOwner = "Jonyboylovespie";
         public const string RepoName = "EverythingIsFree";
