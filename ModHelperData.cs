5467b85e48cfe25d19f998c1d50196a83cc0317c
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2022-12-24 14:58:05 -0500

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@
 {
     public static class ModHelperData
     {
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
         public const string Name = "Everything Is Free";
         public const string Description =
             "This mod makes all upgrades and towers free!";
