0ba744e0db84791aeeb627b9d73666bf6022d866
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2022-12-27 04:42:55 -0500

    Add files via upload

--- EverythingFree.cs
+++ EverythingFree.cs
@@ -1,22 +1,21 @@
-﻿using Assets.Scripts.Models;
+﻿using Il2CppAssets.Scripts.Models;
 using BTD_Mod_Helper;
 using MelonLoader;
-using System;
 using EverythingFree;
-using Assets.Scripts.Models.Towers.Mods;
+using Il2CppAssets.Scripts.Models.Towers.Mods;
 using BTD_Mod_Helper.Api.ModOptions;
 using BTD_Mod_Helper.Api.Data;
 
-[assembly: MelonInfo(typeof(EverythingFree.EverythingFree), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonInfo(typeof(EverythingFree.Main), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 
 namespace EverythingFree
 {
-    public class EverythingFree : BloonsTD6Mod
+    public class Main : BloonsTD6Mod
     {
         public override void OnApplicationStart()
         {
-            MelonLogger.Msg(ConsoleColor.Red, "All upgrades and towers are now free!");
+            MelonLogger.Msg(System.ConsoleColor.Red, "All upgrades and towers are now free!");
         }
         public override void OnNewGameModel(GameModel gameModel, Il2CppSystem.Collections.Generic.List<ModModel> mods)
         {
